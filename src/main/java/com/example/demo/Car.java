c801564f5234d3c1032d72a79cf141e19a4c432b
l3in99 <gkqls99@naver.com>
2024-06-11 13:32:44 +0900

    sd

--- src/main/java/com/example/demo/Car.java
+++ src/main/java/com/example/demo/Car.java
@@ -2,8 +2,17 @@ package com.example.demo;
 
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 @Component // IoC의 대상으로 설정
+@Setter
+@Getter
+@ToString
 public class Car {
+    int tire;
+    String model;
     public void run() {
         System.out.println("Car run!!!");
     }
--- src/main/java/com/example/demo/homecontroller.java
+++ src/main/java/com/example/demo/homecontroller.java
@@ -8,14 +8,22 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class homecontroller {
     @Autowired JdbcTemplate jt;
 
     @RequestMapping("/")
     public String home() {
+        Car car = new Car();
+        car.setTire(3);
+        car.setModel("KIA");
+        log.info(car.toString());
         List<Map<String, Object>> list = jt.queryForList("select * from dept");
         System.out.println(list);
+        log.info(list.toString());
         return "home";
     }
 }
